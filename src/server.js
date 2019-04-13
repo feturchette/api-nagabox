69920fa784e238b9edee51e8a7763f7862e4ff16
Fellipe Turchette <fellipeturchette@hotmail.com>
2019-04-12 22:32:25 -0300

    Updating

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
     req.io = io;
     return next();
 });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/files", express.static(path.resolve(__dirname, "..", "temp")));
@@ -33,4 +34,3 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "temp")));
 app.use(require("./routes"));
 
 server.listen(process.env.PORT || 3333);
-
